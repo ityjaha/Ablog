c3bc7eef3f6be9af29a366fb7ad16d3273566744
yinjun <yinjun@snsoft.com.cn>
2019-08-15 18:08:07 +0800

    修改页面

--- blogadmin/src/views/Container.vue
+++ blogadmin/src/views/Container.vue
@@ -1,28 +1,7 @@
 <template>
   <el-container style="height: 100%">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router :default-openeds="['1']">
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>博客管理</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-    
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
+    <el-header>
+      <el-dropdown>
           <i class="el-icon-setting" style="margin-right: 15px"></i>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>查看</el-dropdown-item>
@@ -31,24 +10,61 @@
           </el-dropdown-menu>
         </el-dropdown>
         <span>王小虎</span>
-      </el-header>
-      
-      <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址">
-          </el-table-column>
-        </el-table>
-      </el-main>
+    </el-header>
+    <el-container>
+      <el-aside width="210px">
+        <el-menu router :default-openeds="['1']" background-color="#545c64" text-color="#fff"  active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title"> <i class="el-icon-location"></i>博客管理</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"> <i class="el-icon-menu"></i>评论管理</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"> <i class="el-icon-document"></i>分类管理</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">  <i class="el-icon-setting"></i>专栏管理</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="日期" width="140">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+          </el-table>
+        </el-main>
     </el-container>
+    <el-footer>京公网安备11111111111111号</el-footer>
   </el-container>
 </template>
 <style>
   .el-header {
-    background-color: #B3C0D1;
+    /*background-color: #B3C0D1;*/
     color: #333;
     line-height: 60px;
   }
